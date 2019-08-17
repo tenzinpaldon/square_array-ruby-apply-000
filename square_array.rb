bd912819ecddc439ff8b46807cc5185cd9ea9a75
Tenzin Paldon <tenzin.paldon16@gmail.com>
2019-08-17 05:38:59 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,5 +1,9 @@
 def square_array(array)
+<<<<<<< HEAD
   new_arr = []
   array.each {|num| new_arr << num*num}
   new_arr
+=======
+  array.collect {|num| num*num}
+>>>>>>> 2f28dae656327979993d05e92c49be5ab9261fc3
 end
